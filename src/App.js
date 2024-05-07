1c4760a8618e49f2b4541e74dbc4835841288fd8
Julia Knura <julia.knura25@gmail.com>
2024-05-07 23:28:44 +0200

    Websocket stub

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {Client} from '@stomp/stompjs'
 
 function App() {
+
+  const connectToSocket = () => {
+    const client = new Client();
+    client.configure({
+      brokerURL: 'ws://localhost:8080/websocket',
+      reconnectDelay: 5000,
+      onConnect: () => {
+        console.log('Connected successfully to websocket');
+
+        //game id and player type hardcoded for test purpose TODO provide real values
+        client.subscribe('/topic/2345/1', message => {
+          const data = JSON.parse(message.body);
+          console.log(data);
+        });
+      },
+      onStompError: (frame) => {
+        console.error('Broker reported error: ' + frame.headers['message']);
+        console.error('Additional details: ' + frame.body);
+      },
+      debug: (str) => {
+        console.log(new Date(), str);
+      }
+    });
+    client.activate();
+  };
+
   return (
     <div className="App">
+      <button onClick={connectToSocket}>Connect</button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
